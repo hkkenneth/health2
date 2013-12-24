98e40c380b8dbc077ce5e9a538ec19f4a8c78b28
Kenneth <hkkenneth@gmail.com>
2013-12-24 17:54:08 +0000

    Initial commit: setup scripts, configs and simple html pages

---
+++ setup/scripts/init.sh
@@ -0,0 +1,6 @@
+#sudo apt-get install git
+#git clone https://github.com/hkkenneth/health2.git
+./health2/setup/scripts/install.sh
+./health2/setup/scripts/start-services.sh
+./health2/setup/scripts/load-configs.sh
+./health2/setup/scripts/restart-services.sh
---
+++ setup/scripts/load-configs.sh
@@ -0,0 +1,1 @@
+sudo cp ../configs/nginx/sites-enabled/default /etc/nginx/sites-enabled/default
